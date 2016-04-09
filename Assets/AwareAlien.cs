17f0d646db9defbb74bfb51764c5cec82474276d
Oscar Losada <oscarlosu@gmail.com>
2016-04-09 11:06:45 +0200

    Initial commit.

---
+++ Assets/AwareAlien.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AwareAlien : MonoBehaviour {
+    public List<GazeAwareComponent> _gazeAware;
+    public Animator Anim;
+
+    private bool lookingBack;
+
+    void Start() {
+        lookingBack = false;
+    }
+
+    void Update() {
+        bool hasGaze = false;
+        foreach(GazeAwareComponent part in _gazeAware) {
+            if(part.HasGaze) {
+                hasGaze = true;
+                break;
+            }
+        }
+        if (hasGaze) {
+            //transform.Rotate(Vector3.forward);
+            Anim.SetBool("Look", true);
+            lookingBack = true;
+            Debug.Log("has gaze");
+        } else if(lookingBack) {
+            Anim.SetBool("Look", false);
+            lookingBack = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/SpinOnGaze.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SpinOnGaze : MonoBehaviour {
+    private GazeAwareComponent _gazeAware;
+
+    void Start() {
+        _gazeAware = GetComponent<GazeAwareComponent>();
+    }
+
+    void Update() {
+        if (_gazeAware.HasGaze) {
+            transform.Rotate(Vector3.forward);
+        }
+    }
+}
