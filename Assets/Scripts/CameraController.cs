95774dd8c429354468b9cd71489b76800575c811
Oscar Losada <oscarlosu@gmail.com>
2016-04-09 16:23:57 +0200

    Camera controller implemented.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ public class CameraController : MonoBehaviour {
     private GazePointDataComponent _gazePointDataComponent;
     private UserPresenceComponent _userPresenceComponent;
     private Light _lightComponent;
+    //private Rigidbody rb;
 
     // Exponential smoothing parameters, alpha must be between 0 and 1.
     [Range(0.1f, 1.0f)]
@@ -14,48 +15,100 @@ public class CameraController : MonoBehaviour {
 
     public Vector2 Margins;
     public float Speed;
+    public AnimationCurve EyeAcceleration;
+    [Range(0.1f, 90.0f)]
+    public float HorizontalMaxAngle;
+    [Range(0.1f, 90.0f)]
+    public float VerticalMaxAngle;
+
+    private Vector2 lookingPos;
+
+
+    public enum LookingState {
+        Right,
+        Left,
+        Up,
+        Down,
+        Center
+    }
+    public LookingState CurrentState;
+    private float elapsedTime;
 
     void Start() {
         _gazePointDataComponent = GetComponent<GazePointDataComponent>();
         _userPresenceComponent = GetComponent<UserPresenceComponent>();
         _lightComponent = GetComponent<Light>();
-        Debug.Log(Screen.width + " " + Screen.height);
+        //rb = GetComponent<Rigidbody>();
+
+        CurrentState = LookingState.Center;
     }
 
+
+    private void HandleState() {
+        LookingState newState = GetNewLookingState();
+        if(CurrentState == LookingState.Center || newState == LookingState.Center) {
+            elapsedTime = 0;
+        }
+        CurrentState = newState;
+        elapsedTime += Time.deltaTime;
+    }
+
+    private LookingState GetNewLookingState() {
+        if(lookingPos.x > 1.0f - Margins.x) {
+            return LookingState.Right;
+        } else if(lookingPos.x < Margins.x) {
+            return LookingState.Left;
+        } else if (lookingPos.y > 1.0f - Margins.y) {
+            return LookingState.Up;
+        } else if (lookingPos.y < Margins.y) {
+            return LookingState.Down;
+        } else {
+            return LookingState.Center;
+        }
+    }
+
+    public bool IsLookingRight() {
+        return lookingPos.x > 1.0f - Margins.x;
+    }
+    public bool IsLookingLeft() {
+        return lookingPos.x < Margins.x;
+    }
+    public bool IsLookingUp() {
+        return lookingPos.y > 1.0f - Margins.y;
+    }
+    public bool IsLookingDown() {
+        return lookingPos.y < Margins.y;
+    }
+
+    private void LookAround() {        
+        // Horizontal
+        if(IsLookingRight()) {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(HorizontalMaxAngle, Vector2.up), Speed * EyeAcceleration.Evaluate(Mathf.Clamp(elapsedTime, 0.0f, 1.0f)) * Time.deltaTime);
+            //rb.angularVelocity = new Vector3(rb.angularVelocity.x, Speed * EyeAcceleration.Evaluate(Mathf.Clamp(elapsedTime, 0.0f, 1.0f)), rb.angularVelocity.z);
+        } else if(IsLookingLeft()) {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(-HorizontalMaxAngle, Vector2.up), Speed * EyeAcceleration.Evaluate(Mathf.Clamp(elapsedTime, 0.0f, 1.0f)) * Time.deltaTime);
+            //rb.angularVelocity = new Vector3(rb.angularVelocity.x, -Speed * EyeAcceleration.Evaluate(Mathf.Clamp(elapsedTime, 0.0f, 1.0f)), rb.angularVelocity.z);
+        }
+        // Vertical
+        if (IsLookingUp()) {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(-VerticalMaxAngle, transform.right), Speed * EyeAcceleration.Evaluate(Mathf.Clamp(elapsedTime, 0.0f, 1.0f)) * Time.deltaTime);
+            //rb.angularVelocity = new Vector3(-Speed * EyeAcceleration.Evaluate(Mathf.Clamp(elapsedTime, 0.0f, 1.0f)), rb.angularVelocity.y, rb.angularVelocity.z);
+        } else if (IsLookingDown()) {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(VerticalMaxAngle, transform.right), Speed * EyeAcceleration.Evaluate(Mathf.Clamp(elapsedTime, 0.0f, 1.0f)) * Time.deltaTime);
+            //rb.angularVelocity = new Vector3(Speed * EyeAcceleration.Evaluate(Mathf.Clamp(elapsedTime, 0.0f, 1.0f)), rb.angularVelocity.y, rb.angularVelocity.z);
+        }
+    }
+
+
     void Update() {
-        var lastGazePoint = _gazePointDataComponent.LastGazePoint;
-
-        if (_userPresenceComponent.IsValid && _userPresenceComponent.IsUserPresent && lastGazePoint.IsValid) {
-            var gazePointInScreenSpace = GetNormalizedGazePosition(lastGazePoint);
-            Debug.Log(gazePointInScreenSpace);
-
-            //Vector3 cameraRotation = transform.localEulerAngles;
-            // Look right
-            if (gazePointInScreenSpace.x > 1.0f - Margins.x) {
-                //transform.RotateAround(transform.position, Vector3.up, Speed * Time.deltaTime);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(90, Vector3.up), Speed * Time.deltaTime);
-
-            }
-            // Look left
-            else if (gazePointInScreenSpace.x < Margins.x) {
-                //transform.RotateAround(transform.position, Vector3.up, -Speed * Time.deltaTime);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(-90, Vector3.up), Speed * Time.deltaTime);
-            }
-            // Look up
-            if (gazePointInScreenSpace.y > 1.0f - Margins.y) {
-                //transform.RotateAround(transform.position, Vector3.right, -Speed * Time.deltaTime);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(-45, Vector3.right), Speed * Time.deltaTime);
-                //cameraRotation.x = Mathf.Clamp(cameraRotation.x, -45, 45);
-            } 
-            // Look down
-            else if(gazePointInScreenSpace.y < Margins.y) {
-                //transform.RotateAround(transform.position, Vector3.right, Speed * Time.deltaTime);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(45, Vector3.right), Speed * Time.deltaTime);
-                //cameraRotation.x = Mathf.Clamp(cameraRotation.x, -45, 45);
-            }
-            // Clamp
-            //transform.localEulerAngles = cameraRotation;
-            
+        EyeXGazePoint gaze = _gazePointDataComponent.LastGazePoint;
+        if (_userPresenceComponent.IsValid && _userPresenceComponent.IsUserPresent && gaze.IsValid) {
+            // Save gaze position
+            lookingPos = GetNormalizedGazePosition(gaze);
+            // Change state and handle elapsed time
+            HandleState();
+            // Change camera rotation
+            LookAround();
 
             _lightComponent.enabled = true;
         } else {
